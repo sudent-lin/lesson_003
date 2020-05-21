f516b76e9afc2c59284fc54c759e6ce48a4c6d5b
IVAL <a_ivanov13@mail.ru>
2020-05-22 00:58:57 +0300

    solve the task 22.05.2020

--- 02_envelop.py
+++ 02_envelop.py
@@ -11,6 +11,7 @@
 
 envelop_x, envelop_y = 10, 7
 paper_x, paper_y = 8, 9
+
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
@@ -21,6 +22,10 @@ paper_x, paper_y = 8, 9
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
+if envelop_x > paper_x and envelop_y > paper_y:
+    print('ДА')
+else:
+    print('НЕТ')
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,8 +33,9 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
+
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
@@ -50,3 +56,11 @@ paper_x, paper_y = 8, 9
 # (просто раскоментировать нужную строку и проверить свой код)
 
 # TODO здесь ваш код
+if hole_x > brick_x and hole_y > brick_y or hole_x > brick_y and hole_y > brick_x:
+    print('ДА')
+elif hole_x > brick_x and hole_y > brick_z or hole_x > brick_z and hole_y > brick_x:
+    print('ДА')
+elif hole_x > brick_y and hole_y > brick_z or hole_x > brick_z and hole_y > brick_y:
+    print('ДА')
+else:
+    print('НЕТ')
