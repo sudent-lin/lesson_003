6e21ad77dbc126237cbf86e38b5cbe0f8926dbf0
ival <a_ivanov13@mail.ru>
2020-05-21 17:27:07 +0300

    enter code 21/05/2020

--- 01_days_in_month.py
+++ 01_days_in_month.py
@@ -12,3 +12,30 @@ month = int(user_input)
 print('Вы ввели', month)
 
 # TODO здесь ваш код
+
+if month == 1:
+    print('31 days in January')
+elif month == 2:
+    print('28 days in February')
+elif month == 3:
+    print('31 days in March')
+elif month == 4:
+    print('30 days in April')
+elif month == 5:
+    print('31 days in May')
+elif month == 6:
+    print('30 days in June')
+elif month == 7:
+    print('31 days in July')
+elif month == 8:
+    print('31 days in August')
+elif month == 9:
+    print('30 days in September')
+elif month == 10:
+    print('31 days in October')
+elif month == 11:
+    print('30 days in November')
+elif month == 12:
+    print('31 days in December')
+else:
+    print(month, 'incorrect input')
